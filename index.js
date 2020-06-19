5cc68b327e065d61ae0b9b017de93e5d3a8407e3
santiracca <santiagoracca7@gmail.com>
2020-06-19 13:44:02 -0400

    Init Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res, next) => {
+  res.send({
+    hi: "there",
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("server running...");
+});
